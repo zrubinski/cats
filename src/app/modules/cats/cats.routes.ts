97582ffaa843c4e2d921a1dc3967a83486fd88e9
Zbigniew Rubinski <z.rubinski@gmail.com>
2024-01-23 20:04:31 +0100

    Cat-fact template component

---
+++ src/app/modules/cats/cats.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { CatFactsComponent } from './pages/cat-facts/cat-facts.component';
+
+export const routes: Route[] = [
+  {
+    path: '',
+    component: CatFactsComponent,
+  },
+];
---
+++ src/app/modules/cats/pages/cat-facts/cat-facts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-facts',
+  standalone: true,
+  imports: [],
+  templateUrl: './cat-facts.component.html',
+  styleUrl: './cat-facts.component.scss',
+})
+export class CatFactsComponent {}
